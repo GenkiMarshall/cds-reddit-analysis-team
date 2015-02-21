6f5fb0d3e6e0351b8ebedd345787bd4a659e7ca4
Genki Marshall <genkimarshall@gmail.com>
2015-02-20 19:36:15 -0500

    Iterate, access data of json response

--- genki/up_subreddit.py
+++ genki/up_subreddit.py
@@ -2,8 +2,12 @@
 
 import requests # http://docs.python-requests.org/en/latest/user/quickstart
 
-url = 'http://www.reddit.com/r/unixporn/top.json?t=month&limit=2'
 header = {'user-agent': 'genki marshall user agent'}
+url = 'http://www.reddit.com/r/unixporn/top.json?t=month&limit=5'
 r = requests.get(url, headers=header)
 
-print(r.json())
+links = r.json()['data']['children']
+for link in links:
+    data = link['data']
+    print(data['title'])
+    print(data['created_utc'])
