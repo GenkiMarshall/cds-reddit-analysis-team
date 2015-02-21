8b736612f1f5e87898604013bf3cd1ce00168fe4
Stepahnie Qian Wang <qw79@cornell.edu>
2015-02-20 19:15:28 -0500

    Code to get reddit "body"

--- Stephanie Wang/2_20_2015.py
+++ Stephanie Wang/2_20_2015.py
@@ -6,6 +6,11 @@
 import urllib2
 
 
+# In[5]:
+
+import json
+
+
 # In[2]:
 
 f = urllib2.urlopen('http://www.reddit.com/r/aww/comments/2vrx59/husky_pup_stealing_from_a_ferret/.json')
@@ -16,24 +21,24 @@ f = urllib2.urlopen('http://www.reddit.com/r/aww/comments/2vrx59/husky_pup_steal
 data = f.read()
 
 
-# In[4]:
-
-data[:50]
-
-
-# In[5]:
-
-import json
-
-
 # In[6]:
 
 parsed_data = json.loads(data)
 
 
-# In[7]:
+# In[18]:
 
-print parsed_data
+object = parsed_data[1]["data"]["children"]
+
+
+# In[58]:
+
+def getBody(object):
+    body = []
+    for child in object: #loop through 31 items in children
+        if child["kind"] == "t1":
+            body.append(child["data"]["body"])
+    return body
 
 
 # In[ ]:
