31c04228c838bf42d0b17cc6b30ad67e4b23cd87
Genki Marshall <genkimarshall@gmail.com>
2015-02-27 18:55:11 -0500

    Get weekday frequency of top 100 posts from year

--- genki/up_subreddit.py
+++ genki/up_subreddit.py
@@ -7,19 +7,25 @@ import requests
 
 # reddit requires user-agent for rate limiting
 header = {'user-agent': 'genki marshall user agent'}
-url = 'http://www.reddit.com/r/unixporn/top.json?t=month&limit=10'
+url = 'http://www.reddit.com/r/unixporn/top.json?t=year&limit=100'
 r = requests.get(url, headers=header)
 links = r.json()['data']['children']
+
+# init array for weekday collection
+weekday_freq = [0,0,0,0,0,0,0]
 
 for link in links:
     data = link['data']
 
-    print(data['title'])
+    # print(data['title'])
 
     local_time_struct = time.localtime(data['created_utc'])
 
     weekday = local_time_struct.tm_wday
-    print('weekday', weekday)
+    # print('weekday', weekday)
+    weekday_freq[weekday] += 1
 
     time_of_day = [local_time_struct.tm_hour, local_time_struct.tm_min]
-    print('time_of_day', time_of_day)
+    # print('time_of_day', time_of_day)
+
+print('weekday_freq', weekday_freq)
