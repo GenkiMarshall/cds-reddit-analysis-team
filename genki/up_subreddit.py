2c01d775652fbec801f9251e8b01b5b41cdc0b72
Genki Marshall <genkimarshall@gmail.com>
2015-02-27 18:45:13 -0500

    Parse out weekday, time of day from links

--- genki/up_subreddit.py
+++ genki/up_subreddit.py
@@ -1,13 +1,25 @@
 #! /bin/python2
 
-import requests # http://docs.python-requests.org/en/latest/user/quickstart
+import time
 
+# http://docs.python-requests.org/en/latest/user/quickstart
+import requests
+
+# reddit requires user-agent for rate limiting
 header = {'user-agent': 'genki marshall user agent'}
-url = 'http://www.reddit.com/r/unixporn/top.json?t=month&limit=5'
+url = 'http://www.reddit.com/r/unixporn/top.json?t=month&limit=10'
 r = requests.get(url, headers=header)
-
 links = r.json()['data']['children']
+
 for link in links:
     data = link['data']
+
     print(data['title'])
-    print(data['created_utc'])
+
+    local_time_struct = time.localtime(data['created_utc'])
+
+    weekday = local_time_struct.tm_wday
+    print('weekday', weekday)
+
+    time_of_day = [local_time_struct.tm_hour, local_time_struct.tm_min]
+    print('time_of_day', time_of_day)
