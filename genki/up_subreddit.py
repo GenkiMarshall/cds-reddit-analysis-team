bb229d6a17df040e909fb7a7fc8c588186ca9509
Genki Marshall <genkimarshall@gmail.com>
2015-02-27 20:03:06 -0500

    Add plotting of data using matplotlib.pyplot

--- genki/up_subreddit.py
+++ genki/up_subreddit.py
@@ -1,15 +1,15 @@
 #! /bin/python2
 
 import time
-
-# http://docs.python-requests.org/en/latest/user/quickstart
 import requests
+import matplotlib.pyplot as plot
 
 # reddit requires user-agent for rate limiting
 header = {'user-agent': 'genki marshall user agent'}
-time_period = 'month'
+#time_period = 'month'
+#time_period = 'year'
+time_period = 'all'
 url = 'http://www.reddit.com/r/unixporn/top.json?t='+time_period+'&limit=100'
-print('time_period', time_period)
 r = requests.get(url, headers=header)
 links = r.json()['data']['children']
 
@@ -19,6 +19,7 @@ hour_of_day_freq = []
 for x in range(24):
     hour_of_day_freq.append(0)
 
+# iterate through links to calculate frequencies
 for link in links:
     data = link['data']
     loc_time = time.localtime(data['created_utc'])
@@ -29,5 +30,9 @@ for link in links:
     hour_of_day = loc_time.tm_hour
     hour_of_day_freq[hour_of_day] += 1
 
-print('weekday_freq', weekday_freq)
 print('hour_of_day_freq', hour_of_day_freq)
+plot.plot(hour_of_day_freq)
+plot.title('time_period: ' + time_period)
+plot.xlabel('hour of day')
+plot.ylabel('frequency')
+plot.show()
