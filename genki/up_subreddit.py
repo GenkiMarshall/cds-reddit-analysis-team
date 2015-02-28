d70328a0bab1d7022020cb2d4d6e7406186b66b6
Genki Marshall <genkimarshall@gmail.com>
2015-02-27 20:52:54 -0500

    Add paging loop to get by 100-post-per-request limit

--- genki/up_subreddit.py
+++ genki/up_subreddit.py
@@ -2,16 +2,10 @@
 
 import time
 import requests
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 
 # reddit requires user-agent for rate limiting
 header = {'user-agent': 'genki marshall user agent'}
-#time_period = 'month'
-#time_period = 'year'
-time_period = 'all'
-url = 'http://www.reddit.com/r/unixporn/top.json?t='+time_period+'&limit=100'
-r = requests.get(url, headers=header)
-links = r.json()['data']['children']
 
 # init arrays for frequency monitoring
 weekday_freq = [0,0,0,0,0,0,0]
@@ -19,20 +13,47 @@ hour_of_day_freq = []
 for x in range(24):
     hour_of_day_freq.append(0)
 
-# iterate through links to calculate frequencies
-for link in links:
-    data = link['data']
-    loc_time = time.localtime(data['created_utc'])
+links = None
+for _ in range(5):
+    # 'limit' is maximum 100 posts at a time
+    url = 'http://www.reddit.com/r/unixporn/top.json?t=all&limit=100'
+    if links is not None:
+        url = url + '&after=' + links[-1]['data']['name']
 
-    weekday = loc_time.tm_wday
-    weekday_freq[weekday] += 1
+    r = requests.get(url, headers=header)
+    links = r.json()['data']['children']
+    for link in links:
+        data = link['data']
+        loc_time = time.localtime(data['created_utc'])
 
-    hour_of_day = loc_time.tm_hour
-    hour_of_day_freq[hour_of_day] += 1
+        weekday = loc_time.tm_wday
+        weekday_freq[weekday] += 1
 
-print('hour_of_day_freq', hour_of_day_freq)
-plot.plot(hour_of_day_freq)
-plot.title('time_period: ' + time_period)
-plot.xlabel('hour of day')
-plot.ylabel('frequency')
-plot.show()
+        hour_of_day = loc_time.tm_hour
+        hour_of_day_freq[hour_of_day] += 1
+
+    # reddit api request rate limit is 30/minute
+    time.sleep(2)
+
+    # print progress
+    print('weekday_freq', weekday_freq)
+    print('hour_of_day_freq', hour_of_day_freq)
+
+# plot weekday monitoring
+weekday_plot = plt.figure(1)
+plt.plot(weekday_freq)
+plt.title('/r/unixporn postings')
+plt.xlabel('weekday (0=Monday)')
+plt.ylabel('frequency')
+weekday_plot.show()
+
+# plot hour_of_day monitoring
+hour_of_day_plot = plt.figure(2)
+plt.plot(hour_of_day_freq)
+plt.title('/r/unixporn postings')
+plt.xlabel('hour of day')
+plt.ylabel('frequency')
+hour_of_day_plot.show()
+
+# keep figures alive after showing
+raw_input()
