4d2f767a0a8aaa8caa3ad9c160fb2f67ab46f474
Genki Marshall <genkimarshall@gmail.com>
2015-02-27 19:08:56 -0500

    Add hour_of_day freq. monitoring of top posts from given time_period

--- genki/up_subreddit.py
+++ genki/up_subreddit.py
@@ -7,25 +7,27 @@ import requests
 
 # reddit requires user-agent for rate limiting
 header = {'user-agent': 'genki marshall user agent'}
-url = 'http://www.reddit.com/r/unixporn/top.json?t=year&limit=100'
+time_period = 'month'
+url = 'http://www.reddit.com/r/unixporn/top.json?t='+time_period+'&limit=100'
+print('time_period', time_period)
 r = requests.get(url, headers=header)
 links = r.json()['data']['children']
 
-# init array for weekday collection
+# init arrays for frequency monitoring
 weekday_freq = [0,0,0,0,0,0,0]
+hour_of_day_freq = []
+for x in range(24):
+    hour_of_day_freq.append(0)
 
 for link in links:
     data = link['data']
+    loc_time = time.localtime(data['created_utc'])
 
-    # print(data['title'])
-
-    local_time_struct = time.localtime(data['created_utc'])
-
-    weekday = local_time_struct.tm_wday
-    # print('weekday', weekday)
+    weekday = loc_time.tm_wday
     weekday_freq[weekday] += 1
 
-    time_of_day = [local_time_struct.tm_hour, local_time_struct.tm_min]
-    # print('time_of_day', time_of_day)
+    hour_of_day = loc_time.tm_hour
+    hour_of_day_freq[hour_of_day] += 1
 
 print('weekday_freq', weekday_freq)
+print('hour_of_day_freq', hour_of_day_freq)
