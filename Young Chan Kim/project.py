50e4f0aaa448017227e091159a6d0769a51d8cef
y2kBoss <Young@dhcp-hol-1866.eduroam.cornell.edu>
2015-03-06 20:26:25 -0500

    frequency of a name in titles, comments, and replies

--- Young Chan Kim/project.py
+++ Young Chan Kim/project.py
@@ -1,66 +1,108 @@
 #February 27, 2015
 
-import urllib2
+#import urllib2
+import requests
 import json
 import time
 
+# Purpose: Plot the frequency of the occurrence the name Westbrook by time of day in the past month
+
+#the string to look for in each title, comment. and reply
+keyword = 'westbrook'
+
+def dfs_Comments_Replies(repliesdict, countdict, name):
+	"""Recursive procedure that traverses through all of the replies found in repliesdict 
+	and maps the keys (hours of day) in countdict to the frequency of the occurrence of 
+	name in each reply.
+	Precondition: repliesdict is either '' or a dictionary containing replies. 
+	countdict is a dictionary containing the hours of day as keys mapped to int values.
+	name is a string.
+	"""
+	#base case (maximum depth reached when 'replies' has value '')
+	if repliesdict == '':
+		return
+	#recursive case
+	replies_children = repliesdict['data']['children']
+	for x in replies_children:
+		if (x['kind'] == 't1'):
+			reply = x['data']['body']
+
+			#get the time of reply and store the hour of day
+			loc_time = time.localtime(x['data']['created_utc'])
+			hour_of_day = loc_time.tm_hour
+
+			countdict[str(hour_of_day)] += count_Frequency(reply, name)
+			
+			dfs_Comments_Replies(x['data']['replies'], countdict, name)
+
+def count_Frequency(s1, s2):
+	"""Return the frequency of s2 occurring in s1.
+	Precondition: s1 and s2 are both strings.
+	"""
+	count = 0
+	for word in s1.split(' '):
+		if s2 in word.lower():
+			count += 1
+	return count
+
+# reddit requires user-agent for rate limiting
+header = {'user-agent': 'Young user agent'}
 #store json data
-request = urllib2.Request('http://www.reddit.com/r/nba/top/.json?sort=top&t=month')
-response = urllib2.urlopen(request)
-data = json.loads(response.read())
-response.close()
+url = 'http://www.reddit.com/r/nba/top/.json?count=100&sort=top&t=month'
+response = requests.get(url, headers=header)
+data  = response.json()
 
 #get the dictionary containing the main links
-children = data[1]['data']['children']
+children = data['data']['children']
 
-#initialize dictionary to store comments
+#initialize dictionary to map hour of day to the frequencies of the name in all titles, 
+#comments, and replies of the subreddit. Of the form {0: 0, 1: 0, ... , 23: 0}.
 comments_dict = {}
-#loop through the children to get each link
+for x in range(24):
+	comments_dict[str(x)] = 0
+
+#loop through the children to access each link
 for c in children:
 	if c['kind'] == 't3':
-		permalink = c['data']['permalink']
-		link = 'http://www.reddit.com' + permalink + '.json?sort=top&t=month'
 		title = c['data']['title']
+
+		#get the time of post and store the hour of day
+		loc_time = time.localtime(c['data']['created_utc'])
+		hour_of_day = loc_time.tm_hour
+
+		comments_dict[str(hour_of_day)] += count_Frequency(title, keyword)
+
+		permalink = c['data']['permalink']
+		link = 'http://www.reddit.com' + permalink + '.json?count=100&sort=top&t=month'
 
 		time.sleep(2) #cannot request more than once every 2 seconds
 
 		#store json data
-		request2 = urllib2.Request(link)
-		response2 = urllib2.urlopen(request2)
-		link_data = json.loads(response2.read())
-		response2.close()
-
-		#initialize list to store this link's comments (and their replies?)
-		this_link_comments = []
+		response2 = requests.get(link, headers=header)
+		link_data = response2.json()
 
 		#get the dictionary containing the main comments
 		link_children = link_data[1]['data']['children']
-		#loop through the children to get each comment
+		#loop through the children to access each comment
 		for lc in link_children:
 			if lc['kind'] == 't1':
 				comment = lc['data']['body']
-				this_link_comments.append(comment)
+
+				#get the time of comment and store the hour of day
+				loc_time = time.localtime(lc['data']['created_utc'])
+				hour_of_day = loc_time.tm_hour
+
+				comments_dict[str(hour_of_day)] += count_Frequency(comment, keyword)
 
 				#get the dictionary containing all the replies to this comment
 				replies = lc['data']['replies']
-				#get each reply
-				
+				#access each reply recursively/iteratively
+				dfs_Comments_Replies(replies, comments_dict, keyword)
 
 
-
-
-		#assign this list of comments as a value to the key (title of the link)
-		comments_dict[title] = this_link_comments
-
-
-
-for t in comments_dict:
-	#parse t (which is the title)
-	for comment in comments_dict[t]:
-		#parse comment
-
-
-
-
+if (comments_dict != {}):
+	for t in comments_dict:
+		print t + ': ' + str(comments_dict[t])
+		
 
 
