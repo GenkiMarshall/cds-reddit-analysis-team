ddbda824a5c2515caab1bf4d161cf891d687c699
y2kBoss <Young@dhcp-hol-286.eduroam.cornell.edu>
2015-03-13 19:15:18 -0400

    Plotting Frequency vs Hour of Day

--- Young Chan Kim/project.py
+++ Young Chan Kim/project.py
@@ -1,22 +1,25 @@
-#March 06, 2015
+#March 13, 2015
 
-#import urllib2
 import requests
 import json
 import time
+import matplotlib.pyplot as plt
 
-# Purpose: Plot the frequency of the occurrence the name Westbrook by time of day in the past month
+# Purpose: Plot the frequency of the occurrence of a certain word or name by time of 
+# day in the past month
 
 #the string to look for in each title, comment. and reply
-keyword = 'westbrook'
+keyword = raw_input('Type an NBA player\'s first or last name, a team name, or a word: ')
+#the title of the plot that will be displayed
+plot_title = 'Frequency of ' + keyword + ' by Hour of Day in the Past Month\'s NBA Subreddit'
 
-def dfs_Comments_Replies(repliesdict, countdict, name):
+def dfs_Comments_Replies(repliesdict, countlist, word):
 	"""Recursive procedure that traverses through all of the replies found in repliesdict 
-	and maps the keys (hours of day) in countdict to the frequency of the occurrence of 
-	name in each reply.
+	and maps the hours of day (the corresponding indices) in countlist to the frequency of 
+	the occurrence of word in each reply.
 	Precondition: repliesdict is either '' or a dictionary containing replies. 
-	countdict is a dictionary containing the hours of day as keys mapped to int values.
-	name is a string.
+	countlist is a list containing the hours of day as indices mapped to int values.
+	word is a string.
 	"""
 	#base case (maximum depth reached when 'replies' has value '')
 	if repliesdict == '':
@@ -31,9 +34,9 @@ def dfs_Comments_Replies(repliesdict, countdict, name):
 			loc_time = time.localtime(x['data']['created_utc'])
 			hour_of_day = loc_time.tm_hour
 
-			countdict[str(hour_of_day)] += count_Frequency(reply, name)
+			countlist[hour_of_day] += count_Frequency(reply, word)
 			
-			dfs_Comments_Replies(x['data']['replies'], countdict, name)
+			dfs_Comments_Replies(x['data']['replies'], countlist, word)
 
 def count_Frequency(s1, s2):
 	"""Return the frequency of s2 occurring in s1.
@@ -41,7 +44,7 @@ def count_Frequency(s1, s2):
 	"""
 	count = 0
 	for word in s1.split(' '):
-		if s2 in word.lower():
+		if s2.lower() in word.lower():
 			count += 1
 	return count
 
@@ -55,11 +58,11 @@ data  = response.json()
 #get the dictionary containing the main links
 children = data['data']['children']
 
-#initialize dictionary to map hour of day to the frequencies of the name in all titles, 
-#comments, and replies of the subreddit. Of the form {0: 0, 1: 0, ... , 23: 0}.
-comments_dict = {}
+#initialize list to map hour of day to the frequencies of the word in all titles, 
+#comments, and replies of the subreddit.
+comments_list = []
 for x in range(24):
-	comments_dict[str(x)] = 0
+	comments_list.append(0)
 
 #loop through the children to access each link
 for c in children:
@@ -70,7 +73,7 @@ for c in children:
 		loc_time = time.localtime(c['data']['created_utc'])
 		hour_of_day = loc_time.tm_hour
 
-		comments_dict[str(hour_of_day)] += count_Frequency(title, keyword)
+		comments_list[hour_of_day] += count_Frequency(title, keyword)
 
 		permalink = c['data']['permalink']
 		link = 'http://www.reddit.com' + permalink + '.json?count=100&sort=top&t=month'
@@ -92,17 +95,20 @@ for c in children:
 				loc_time = time.localtime(lc['data']['created_utc'])
 				hour_of_day = loc_time.tm_hour
 
-				comments_dict[str(hour_of_day)] += count_Frequency(comment, keyword)
+				comments_list[hour_of_day] += count_Frequency(comment, keyword)
 
 				#get the dictionary containing all the replies to this comment
 				replies = lc['data']['replies']
 				#access each reply recursively/iteratively
-				dfs_Comments_Replies(replies, comments_dict, keyword)
+				dfs_Comments_Replies(replies, comments_list, keyword)
 
 
-if (comments_dict != {}):
-	for t in comments_dict:
-		print t + ': ' + str(comments_dict[t])
-		
+
+if (comments_list != []):
+	plt.plot(comments_list)
+	plt.title(plot_title)
+	plt.xlabel('Hour of Day')
+	plt.ylabel('Frequency')
+	plt.show()
 
 
