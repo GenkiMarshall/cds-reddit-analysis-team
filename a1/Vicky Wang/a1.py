1b7c94b458ff2df52f007060bdc8529713f08fac
vickywang8 <vickyminzhi@gmail.com>
2015-04-10 18:53:48 -0400

    two urls

--- a1/Vicky Wang/a1.py
+++ a1/Vicky Wang/a1.py
@@ -11,13 +11,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import requests
 
-url = 'http://www.reddit.com/user/' + name + '/about.json'
+url_user = 'http://www.reddit.com/user/' + username + '/about.json'
 header = {'user-agent': 'Vicky user-agent'}
-r = requests.get(url, headers=header).json()
-data = r['data']
+user_json = requests.get(url_user, headers=header).json()
+user_data = user_json['data']
 
-is_mod = data['is_mod']
-is_verified = data['has_verified_email']
-is_gilded = data['is_gold']
-author = data['name']
-created_utc = data['created_utc']
+is_mod = user_data['is_mod']
+is_verified = user_data['has_verified_email']
+is_gilded = user_data['is_gold']
+author = user_data['name']
+created_utc = user_data['created_utc']
+link_karma = user_data['link_karma']
+comment_karma = user_data['comment_karma']
+
+url_comment = 'http://www.reddit.com/user/' + username + '/comments/json'
+header = {'user-agent': 'Vicky user-agent'}
+comment_json = requests.get(url_comment, headers=header).json()
+comment_data = comment_json['data']
+
+allcomments = comment_data['children']['data']
