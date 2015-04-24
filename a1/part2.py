799834587d5e9864bf66e0c9e703efc34063331c
Genki Marshall <genkimarshall@gmail.com>
2015-04-24 19:22:23 -0400

    Add num_replies_before_this_reply

--- a1/part2.py
+++ a1/part2.py
@@ -43,19 +43,39 @@ def get_meta_data(data, section): return data[section]['data']['children'][0]['d
 
 # IMPL HELPERS
 
-# iterates through all children of parent to find the number of ...
-# ... direct replies before created_utc
+# iterates through all direct replies to given parent to find ...
+# the number of direct replies before created_utc
 # does the comment_tree looper
-def gnpr_helper(comment_tree, created_utc):
-    for comment in comment_tree:
-        data = comment['data']
-        if comment['kind'] == 'more':
-            print('more', data['count'])
-        else:
-            print(data['score'])
+def get_num_prior_replies(parent_fullname, orig_utc):
 
-# main
-def get_num_prior_replies(parent_fullname, created_utc):
+    # helper for when finds "more" objects
+    # checks if the comment of the given comment_id was posted before orig_utc
+    def check_if_earlier(comment_id):
+        data = requests.get(make_url(LINK_EX, comment_id), headers=header).json()
+        return get_meta_data(data, 1)['created_utc'] < orig_utc
+
+    # comment_tree has the given parent as the root
+    def gnpr_helper(comment_tree):
+        num_prior_replies = 0
+        for comment in comment_tree:
+            data = comment['data']
+            if comment['kind'] == 'more':
+                # these 'more' objects contain more comments to iterate through
+                children = data['children']
+                children_len = len(children)
+                print('found a more-object with ' + `children_len` + ' children...')
+                if children_len > 10:
+                    print('this is too many to iterate, so only the first ten will be checked')
+                for idx,c in enumerate(children):
+                    if idx < 10:
+                        print('checking more-child #' + `idx+1` + '...')
+                        time.sleep(2)
+                        if check_if_earlier(c): num_prior_replies += 1
+            else:
+                curr_utc = data['created_utc']
+                if curr_utc < orig_utc: num_prior_replies += 1
+        return num_prior_replies
+
     parent_id = rm_type(parent_fullname)
     is_link = is_link_name(parent_fullname)
     if is_link:
@@ -63,7 +83,7 @@ def get_num_prior_replies(parent_fullname, created_utc):
     else:
         parent_url = make_url(LINK_EX, parent_id)
     data = requests.get(parent_url, headers=header).json()
-    return gnpr_helper(get_replies(data, is_link), created_utc)
+    return gnpr_helper(get_replies(data, is_link))
 
 # MAIN
 def get_data(url, is_comment_url):
@@ -83,10 +103,10 @@ def get_data(url, is_comment_url):
         # ... and were posted before this
         num_prior_replies = get_num_prior_replies(
             parent_fullname, meta_data['created_utc'])
-        print(num_prior_replies)
+        print("num_replies_before_this_reply", num_prior_replies)
     else:
         print('analyzing link: ' + url)
 
 get_data(make_url(LINK_EX, COMMENT_EX_1), True)
 time.sleep(2)
-get_data(make_url(LINK_EX, COMMENT_EX_2), True)
+# get_data(make_url(LINK_EX, COMMENT_EX_2), True)
